f8d57cc519b73e6901a4db3d2f74ad39af48b02e
matathias <warrick.david58@gmail.com>
2026-06-22 16:55:02 -0700

    Remove the 'reroute power' option from the Power Failure event

--- 1.6/Source/Core/MoreEventsDefOf.cs
+++ 1.6/Source/Core/MoreEventsDefOf.cs
@@ -5,8 +5,6 @@ namespace FactionColonies.Events
     [DefOf]
     public static class MoreEventsDefOf
     {
-        public static FCEventDef empireEvents_powerFailure_reroute_main;
-        public static FCEventDef empireEvents_powerFailure_reroute_drain;
         public static FCEventDef empireEvents_goldenAge;
         public static FCEventDef empireEvents_growingPains_0;
         public static FCEventDef empireEvents_tribute_0;
